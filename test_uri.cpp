2f7dba2218b0708704064c697144a10025dced86
Alexandra Parker <alex.iris.parker@gmail.com>
2022-05-21 13:45:39 -0700

    refactor: cooler looking test

--- test_uri.cpp
+++ test_uri.cpp
@@ -2,9 +2,10 @@
 
 #include "uri.hpp"
 
-template<typename Os>
-auto& operator<<(Os &os, std::source_location loc) {
-  return os << loc.file_name() << ':' << loc.function_name() << ':' << loc.line() << ':' << loc.column();
+template <typename Os>
+auto& operator<<(Os& os, std::source_location loc) {
+  return os << loc.file_name() << ':' << loc.function_name() << ':'
+            << loc.line() << ':' << loc.column();
 }
 
 template <typename T>
@@ -14,20 +15,20 @@ struct expecter {
 
   template <typename U>
   void operator==(U expected) {
-    if(got != expected) {
+    if (got != expected) {
       cerr << loc << " Expected: " << expected << " Got: " << got << '\n';
       std::exit(1);
     }
   }
 };
 
-template<>
+template <>
 struct expecter<bool> {
   bool got;
   std::source_location loc;
 
   ~expecter() {
-    if(!got) {
+    if (!got) {
       cerr << loc << ": Expected: true Got: false\n";
       std::exit(1);
     }
@@ -39,89 +40,45 @@ auto expect(T got, std::source_location loc = std::source_location::current()) {
   return expecter{got, loc};
 }
 
+void test_uri_view() {
+  using data_t = struct { string_view scheme, host, port, path, query; };
 
-int main() {
-  UriView u{"gemini://example.com:1966/foo/bar?asdf=zxcv&hjkl=vbnm"};
+  auto test_one = [](string_view uri, data_t data,
+                     std::source_location loc =
+                         std::source_location::current()) {
+    UriView u{uri};
+    expect(u.scheme(), loc) == data.scheme;
+    expect(u.host(), loc) == data.host;
+    expect(u.port(), loc) == data.port;
+    expect(u.path(), loc) == data.path;
+    expect(u.query(), loc) == data.query;
+  };
 
-  expect(u.scheme()) == "gemini";
-  expect(u.host()) == "example.com";
-  expect(u.port()) == "1966";
-  expect(u.path()) == "/foo/bar";
-  expect(u.query()) == "asdf=zxcv&hjkl=vbnm";
-
-  u = UriView{"gemini://[::1]:1966/foo/bar?asdf=zxcv&hjkl=vbnm"};
-
-  expect(u.scheme()) == "gemini";
-  expect(u.host()) == "[::1]";
-  expect(u.port()) == "1966";
-  expect(u.path()) == "/foo/bar";
-  expect(u.query()) == "asdf=zxcv&hjkl=vbnm";
-
-  u = UriView{"foo/bar"};
-  expect(u.scheme()) == "";
-  expect(u.host()) == "";
-  expect(u.port()) == "";
-  expect(u.path()) == "foo/bar";
-  expect(u.query()) == "";
-
-  u = UriView{"//asdf:123:"};
-  expect(u.scheme()) == "";
-  expect(u.host()) == "asdf:123";
-  expect(u.port()) == "";
-  expect(u.path()) == "";
-  expect(u.query()) == "";
-
-  u = UriView{"//asdf/foo/bar"};
-  expect(u.scheme()) == "";
-  expect(u.host()) == "asdf";
-  expect(u.port()) == "";
-  expect(u.path()) == "/foo/bar";
-  expect(u.query()) == "";
-
-  u = UriView{"gemini:foo/bar"};
-  expect(u.scheme()) == "gemini";
-  expect(u.host()) == "";
-  expect(u.path()) == "foo/bar";
-
-  u = UriView{"gemini:"};
-  expect(u.scheme()) == "gemini";
-  expect(u.host()) == "";
-  expect(u.port()) == "";
-  expect(u.path()) == "";
-  expect(u.query()) == "";
-
-  u = UriView{":::::"};
-  expect(u.scheme()) == "";
-  expect(u.host()) == "";
-  expect(u.port()) == "";
-  expect(u.path()) == "::::";
-  expect(u.query()) == "";
-
-  u = UriView{""};
-  expect(u.scheme()) == "";
-  expect(u.host()) == "";
-  expect(u.port()) == "";
-  expect(u.path()) == "";
-  expect(u.query()) == "";
-
-  u = UriView{"/////"};
-  expect(u.scheme()) == "";
-  expect(u.host()) == "";
-  expect(u.port()) == "";
-  expect(u.path()) == "///";
-  expect(u.query()) == "";
-
-  u = UriView{"foo:?asdf=zxcv"};
-  expect(u.scheme()) == "foo";
-  expect(u.host()) == "";
-  expect(u.port()) == "";
-  expect(u.path()) == "";
-  expect(u.query()) == "asdf=zxcv";
-
-  u = UriView{"foo:/?asdf=zxcv"};
-  expect(u.scheme()) == "foo";
-  expect(u.host()) == "";
-  expect(u.port()) == "";
-  expect(u.path()) == "/";
-  expect(u.query()) == "asdf=zxcv";
+  test_one("gemini://example.com:1966/foo/bar?asdf=zxcv&hjkl=vbnm", {
+    scheme : "gemini",
+    host : "example.com",
+    port : "1966",
+    path : "/foo/bar",
+    query : "asdf=zxcv&hjkl=vbnm"
+  });
+  test_one("gemini://[::1]:1966/foo/bar?asdf=zxcv&hjkl=vbnm", {
+    scheme : "gemini",
+    host : "[::1]",
+    port : "1966",
+    path : "/foo/bar",
+    query : "asdf=zxcv&hjkl=vbnm"
+  });
+  test_one("foo/bar", {path : "foo/bar"});
+  test_one("//asdf:123:", {host : "asdf:123"});
+  test_one("//asdf/foo/bar", {host : "asdf", path : "/foo/bar"});
+  test_one("gemini:foo/bar", {scheme : "gemini", path : "foo/bar"});
+  test_one("gemini:", {scheme : "gemini"});
+  test_one(":::::", {path : "::::"});
+  test_one("", {});
+  test_one("/////", {path : "///"});
+  test_one("foo:?asdf=zxcv", {scheme : "foo", query : "asdf=zxcv"});
+  test_one("foo:/?asdf=zxcv",
+           {scheme : "foo", path : "/", query : "asdf=zxcv"});
 }
+
+int main() { test_uri_view(); }

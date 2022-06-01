4c9691cc70595fdd0fe7abaa63e8eec93afb2b86
Alexandra Parker <alex.iris.parker@gmail.com>
2022-05-31 22:24:20 -0700

    refactor(ossl): docs, forwarding ctors

--- openssl.hpp
+++ openssl.hpp
@@ -5,6 +5,7 @@
 
 namespace openssl {
 
+namespace detail {
 template <typename Ptr, int(UpRef)(Ptr), void(Free)(Ptr)>
 requires std::is_pointer_v<Ptr>
 class Refcnt {
@@ -28,29 +29,67 @@ class Refcnt {
   Ptr p;
 };
 
+}  // namespace detail
+
+/// Reference-counted pointer to an OpenSSL session (SSL_SESSION*).
+/// Always increments the ref count on initialization. How to borrow?
 class Session {
  public:
-  using ptr_t = Refcnt<SSL_SESSION*, SSL_SESSION_up_ref, SSL_SESSION_free>;
+  using ptr_t =
+      detail::Refcnt<SSL_SESSION*, SSL_SESSION_up_ref, SSL_SESSION_free>;
 
-  Session(ptr_t ptr) : p{ptr} {}
+  template<typename T>
+  Session(T&& t) : p{std::move(t)} {}
+
+  //// True if this wraps a non-null handle.
   operator bool() { return *p; }
 
+  /** Get the servername from the SNI extension.
+   *
+   * @par
+   * Returns a pointer to a C string containing the TLS server
+   * name if set, or nullptr otherwise.
+   * 
+   * @remarks This only works on TLS 1.2 and below.
+   */
   const char* hostname() { return SSL_SESSION_get0_hostname(*p); }
 
  private:
   ptr_t p;
 };
 
+/** Reference-counted pointer to an OpenSSL connection (SSL*).
+ *
+ * @par
+ * Always increments the ref count on initialization. How to borrow?
+ */
 class Ssl {
  public:
-  using ptr_t = Refcnt<SSL*, SSL_up_ref, SSL_free>;
-  Ssl(ptr_t ptr) : p{ptr} {}
+  using ptr_t = detail::Refcnt<SSL*, SSL_up_ref, SSL_free>;
+
+  template<typename T>
+  Ssl(T&& t) : p{std::move(t)} {}
+
+  //// True if this wraps a non-null handle.
   operator bool() { return *p; }
 
+  /** Get the servername from the SNI extension.
+   *
+   * @par
+   * Returns a pointer to a C string containing the TLS server
+   * name if set, or nullptr otherwise.
+   */
   const char* servername() {
     auto type{SSL_get_servername_type(*p)};
     return type < 0 ? nullptr : SSL_get_servername(*p, type);
   }
+
+  /**
+   * @brief gets a shared reference to this connection's session.
+   *
+   * session() returns a pointer to a C string containing the TLS server
+   * name if set, or nullptr otherwise.
+   */
   Session session() { return Session{SSL_get_session(*p)}; }
 
  private:
